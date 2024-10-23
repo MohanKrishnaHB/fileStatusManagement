0efd8ef85bf955f80a3f58355d7fb2b9869a7337
Mohan Krishna <mohankrishnahb@gmail.com>
2024-10-23 23:05:40 +0530

    Created Models

--- fileStatusManagement/client/models.py
+++ fileStatusManagement/client/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    ROLES_CHOICES = [
+        ('champ', 'Champ'),
+        ('lead', 'Lead'),
+        ('client', 'Client'),
+    ]
+    role = models.CharField(max_length=20, choices=ROLES_CHOICES)
+
+    def __str__(self):
+        return self.email
+
+class Client(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    
+    def __str__(self):
+        return self.name
+
+class ClientLogin(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    clientUser = models.ForeignKey(CustomUser, on_delete=models.CASCADE, unique=True)
+    
+    def __str__(self):
+        return self.client + ' | ' + self.clientUser
--- fileStatusManagement/file/models.py
+++ fileStatusManagement/file/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from client.models import Client, CustomUser
 
-# Create your models here.
+class Status(models.Model):
+    COLORS_CHOICES = [
+        ('green', 'Green'),
+        ('blue', 'Blue'),
+        ('yellow', 'Yellow'),
+        ('red', 'Red'),
+    ]
+    title = models.CharField(max_length=500, unique=True)
+    color = models.CharField(max_length=20, choices=COLORS_CHOICES)
+    
+    def __str__(self):
+        return self.title
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    customerId = models.CharField(max_length=100, unique=True)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name + '-' + self.customerId
+
+class File(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class FileStatus(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    file = models.ForeignKey(File, on_delete=models.CASCADE)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE)
+    comments = models.CharField(max_length=500)
+    date = models.DateField()
+    drafterBy = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    isDraft = models.BooleanField(default=True)
