32cbc52de2fe341aaa66fe287aac10b475242bbb
Mohan Krishna <mohankrishnahb@gmail.com>
2024-10-26 02:16:05 +0530

    added status page design

--- fileStatusManagement/file/urls.py
+++ fileStatusManagement/file/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import dashboard
+from .views import dashboard, status
 
 app_name = 'file'
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),
+    path('status/', status, name='status'),
 ]
--- fileStatusManagement/file/views.py
+++ fileStatusManagement/file/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from client.models import Client
+from .models import Customer, File, FileStatus, Status
+from datetime import date
 
 @login_required
 def dashboard(request):
@@ -11,4 +14,26 @@ def dashboard(request):
         return render(request, 'client/dashboard.html')
     else:
         logout(request)
-        return render(request, 'login.html', {'error': 'Access Denied'})
+        return redirect('/admin')
+
+@login_required
+def status(request):
+    user = request.user
+    if user.isChamp():
+        clients = Client.objects.all()
+        customers = Customer.objects.filter(client=clients[0].id).values()
+        files = File.objects.filter(client=clients[0].id).values()
+        status = Status.objects.all()
+        retrunObj = {
+            'clients': clients,
+            'customers': customers,
+            'files': files,
+            'status': status,
+            'date': date.today().strftime("%Y-%m-%d")
+        }
+        return render(request, 'champ/status.html', retrunObj)
+    elif user.isClient():
+        return render(request, 'client/status.html')
+    else:
+        logout(request)
+        return redirect('/admin')
