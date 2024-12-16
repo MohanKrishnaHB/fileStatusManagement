b74e50d1882a2314c6734e7092a4cdda813b0893
Mohan Krishna <mohankrishnahb@gmail.com>
2024-12-16 10:32:00 +0530

    updated deployment changes

--- fileStatusManagement/settings.py
+++ fileStatusManagement/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u6o*cz1r)w_wyu4=!+2htt!pdu2@lzc9%*9g)62cn2+_%&e1+*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -84,7 +83,6 @@ DATABASES = {
 }
 
 
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
@@ -120,7 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Default primary key field type
